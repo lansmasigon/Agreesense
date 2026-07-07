cd888f20bd2e2935360842b1ae7def052c06a7f5
lansmasigon <lancemasigon@gmail.com>
2026-07-07 19:12:41 +0800

    Fix
    Vercel

---
+++ web_portal/vercel.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable
+export PATH="$PATH:`pwd`/flutter/bin"
+
+flutter config --enable-web
+flutter pub get
+flutter build web
