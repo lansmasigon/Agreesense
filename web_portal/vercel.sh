85c3dfbd81f463640473c471d9f2d7aef7b82a6b
lansmasigon <lancemasigon@gmail.com>
2026-07-07 19:32:14 +0800

    UPDATE
    vercel 1

--- web_portal/vercel.sh
+++ web_portal/vercel.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -e
 
+echo "Installing Flutter..."
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable
-export PATH="$PATH:`pwd`/flutter/bin"
+export PATH="$PATH:$(pwd)/flutter/bin"
+
+echo "Creating .env..."
+cat > .env <<EOF
+SUPABASE_URL=$SUPABASE_URL
+SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY
+EOF
 
 flutter config --enable-web
 flutter pub get
-flutter build web
+flutter build web --release
